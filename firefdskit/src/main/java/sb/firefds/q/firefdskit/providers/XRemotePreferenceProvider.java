8b5a81f2a752c1a4e293630d39a7becb41f0d5a2
Shauli Bracha <shauli.bracha@hotmail.com>
2020-02-25 00:26:52 -0500

    Fixed RemotePreferences implementation for Q

--- firefdskit/src/main/java/sb/firefds/q/firefdskit/providers/XRemotePreferenceProvider.java
+++ firefdskit/src/main/java/sb/firefds/q/firefdskit/providers/XRemotePreferenceProvider.java
@@ -4,10 +4,11 @@ import com.crossbowffs.remotepreferences.RemotePreferenceFile;
 import com.crossbowffs.remotepreferences.RemotePreferenceProvider;
 
 import static sb.firefds.q.firefdskit.utils.Constants.PREFS;
+import static sb.firefds.q.firefdskit.utils.Constants.PREFS_AUTHORITY;
 
 public class XRemotePreferenceProvider extends RemotePreferenceProvider {
     public XRemotePreferenceProvider() {
-        super("sb.firefds.q.firefdskit.preferences", new RemotePreferenceFile[]{
+        super(PREFS_AUTHORITY, new RemotePreferenceFile[]{
                 new RemotePreferenceFile(PREFS, true)});
     }
 }
